c0c55e7722b180ddf81428eed4ad1c85aa764516
ali <m4567s019283@gmail.com>
2025-11-01 21:37:08 +0200

    commit

--- front-end/src/assets/assets.js
+++ front-end/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './logo.png'
+
 import sample_cover from './sample_cover.jpg'
 import sample_profile from './sample_profile.jpg'
 import bgImage from './bgImage.png'
@@ -7,7 +7,6 @@ import { Home, MessageCircle, Search, UserIcon, Users } from 'lucide-react'
 import sponsored_img from './sponsored_img.png'
 
 export const assets = {
-    logo,
     sample_cover,
     sample_profile,
     bgImage,
